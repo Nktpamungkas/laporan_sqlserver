4ecc2dce8ecd46bf06d80ff0e1be2e79852f95e7
Nktpamungkas <nktpamungkas28@gmail.com>
2024-09-17 08:50:38 +0700

    $con_db_lab

--- api_laboratmatching.php
+++ api_laboratmatching.php
@@ -1,21 +1,21 @@
 <?php
-include 'koneksi.php';
+    include 'koneksi.php';
 
-$nowarna    = $_GET['no_warna'];
-// Syntax MySql untuk melihat semua record yang
-$sql = "SELECT DISTINCT no_warna, warna, langganan FROM db_laborat.tbl_matching WHERE recipe_code = '$nowarna'";
+    $nowarna    = $_GET['no_warna'];
+    // Syntax MySql untuk melihat semua record yang
+    $sql = "SELECT DISTINCT no_warna, warna, langganan FROM `tbl_matching` WHERE recipe_code = '$nowarna'";
 
-//Execetute Query diatas
-$query = sqlsrv_query($con_db_lab, $sql);
-$dt    = sqlsrv_fetch_array($query);
+    //Execetute Query diatas
+    $query = mysqli_query($con_db_lab, $sql);
+    $dt    = mysqli_fetch_assoc($query);
 
-//Menampung data yang dihasilkan
-$json = array(
-    'no_warna'      => $dt['no_warna'],
-    'warna'         => $dt['warna'],
-    'langganan'     => $dt['langganan']
-);
+    //Menampung data yang dihasilkan
+    $json = array(
+        'no_warna'      => $dt['no_warna'],
+        'warna'         => $dt['warna'],
+        'langganan'     => $dt['langganan']
+    );
 
-//Merubah data kedalam bentuk JSON
-header('Content-Type: application/json');
-echo json_encode($json);
+    //Merubah data kedalam bentuk JSON
+    header('Content-Type: application/json');
+    echo json_encode($json);
