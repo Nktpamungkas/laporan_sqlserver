e8a14b78c8b27d5ac975350f956f2e8332a3bbdd
Nktpamungkas <nktpamungkas28@gmail.com>
2024-10-10 16:50:20 +0700

    matiin fungsi

--- footer.php
+++ footer.php
@@ -544,7 +544,7 @@
                 dyelot.Color || "",
                 `${dyelot.ImportState || ""} ${badge}`,
                 dyelot.ImportState == 10 ? `<button class="btn btn-danger" id="update-btn" data-dyelot="${dyelot.Dyelot}" data-redye="${dyelot.ReDye}" data-importstate="30">Delete Batch</button>` :
-                dyelot.ImportState == 10 ? `<button class="btn btn-danger" id="update-btn" data-dyelot="${dyelot.Dyelot}" data-redye="${dyelot.ReDye}" data-importstate="40">Delete Batch</button>` :
+                // dyelot.ImportState == 10 ? `<button class="btn btn-danger" id="update-btn" data-dyelot="${dyelot.Dyelot}" data-redye="${dyelot.ReDye}" data-importstate="40">Delete Batch</button>` :
                 ''
               ];
             });
