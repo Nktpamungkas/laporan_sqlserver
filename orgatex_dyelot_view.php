5aebb8de517ecb9a8ec445aeaf24bb78057d421f
Nktpamungkas <nktpamungkas28@gmail.com>
2024-10-16 16:00:22 +0700

    ganti warna jika importstate 20

--- orgatex_dyelot_view.php
+++ orgatex_dyelot_view.php
@@ -139,7 +139,7 @@ require_once "koneksi.php";
                                                         } else if ($dyelot['ImportState'] == 50) {
                                                             $badge = '<span class="badge badge-pill badge-danger p-2">Error Delete</span>';
                                                         } else if ($dyelot['ImportState'] == 20) {
-                                                            $badge = '<span class="badge badge-pill badge-danger p-2">Error Import</span>';
+                                                            $badge = '<span class="badge badge-pill badge-dark p-2">Error Import</span>';
                                                         } else {
                                                             $badge = '';
                                                         }
